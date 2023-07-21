dd72872bd2269ff491a90179952ab1ee32ce06d1
Иван Харитонов <haritonoviwan@yandex.ru>
2023-07-21 11:22:21 +0400

    Сделал второе задание

---
+++ ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine() ?? "");
+
+if (firstNumber > secondNumber)
+{
+    Console.Write($"max = {firstNumber}");
+}
+
+else if (firstNumber < secondNumber)
+{
+    Console.Write($"max = {secondNumber}");
+}
+
+else 
+{
+    Console.Write("Числа равны");
+}
