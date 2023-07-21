320d70b89f552f2d29dfc2c7b7103979cd80aa9b
Иван Харитонов <haritonoviwan@yandex.ru>
2023-07-21 15:43:25 +0400

    Сделал восьмое задание

---
+++ ex8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine($"Все четные числа от 1 до {number}: ");
+
+int count = 2;
+while (count <= number)
+{
+    if (count % 2 == 0)
+    {
+        Console.WriteLine(count);
+    }
+    count = count + 1;
+}
