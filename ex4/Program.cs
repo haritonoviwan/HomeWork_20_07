5617dfe0205c8942343749be3ca53e781ce15cc2
Иван Харитонов <haritonoviwan@yandex.ru>
2023-07-21 11:56:29 +0400

    Сделал четвертое задание

---
+++ ex4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите третье число: ");
+int thirdNumber = int.Parse(Console.ReadLine() ?? "");
+
+int maxNumber = firstNumber;
+
+if (secondNumber > maxNumber)
+{
+    maxNumber = secondNumber;
+}
+
+if (thirdNumber > maxNumber)
+{
+    maxNumber = thirdNumber;
+}
+
+Console.Write($"{firstNumber}, {secondNumber}, {thirdNumber} -> {maxNumber}");
